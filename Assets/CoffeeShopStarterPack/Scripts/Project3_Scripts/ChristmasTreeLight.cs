ed8d779a5cbe5d8e9d0e269501f6b9f42c2c9cac
Advait Pai <67473396+advaitpai@users.noreply.github.com>
2022-11-22 21:55:39 -0600

    Update ChristmasTreeLight.cs

--- Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ChristmasTreeLight.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ChristmasTreeLight.cs
@@ -7,15 +7,12 @@ public class ChristmasTreeLight : MonoBehaviour
     Material lights;
     public bool is_on;
     public GameObject obj;
-    // public Transform[] children;
-    // Start is called before the first frame update
+    
     public void Start()
     {
          
          
          lights = obj.GetComponent<Renderer>().sharedMaterial;
-        //  children = obj.GetComponentsInChildren<Transform>();
-        //  Debug.Log(children);
          is_on = false;
     }
 
@@ -24,22 +21,10 @@ public class ChristmasTreeLight : MonoBehaviour
         is_on = !is_on;
         if(is_on)
         {
-            // foreach(Transform child in children)
-            // {
-            //     // GameObject temp = child.gameObject;
-            //     // Debug.Log(temp.name);
-            //     // Material lights = temp.material;
-            //     // lights.EnableKeyword("_EMISSION");
-            //     //Debug.Log(child);
-            // }
             lights.EnableKeyword("_EMISSION");
         }
         else
         {
-            //  foreach(Transform child in children)
-            // {
-            //     child.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-            // }
             lights.DisableKeyword("_EMISSION");
         }
         
