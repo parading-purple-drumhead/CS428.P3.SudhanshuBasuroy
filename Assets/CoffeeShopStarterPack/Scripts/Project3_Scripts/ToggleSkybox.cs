308f570d02483b539e4ecea1eff24b52b86840b4
Advait Pai <67473396+advaitpai@users.noreply.github.com>
2022-11-26 03:10:37 -0600

    Night Logic Started

--- Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ToggleSkybox.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ToggleSkybox.cs
@@ -1,36 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zinnia.Tracking.Collision;
 
 public class ToggleSkybox : MonoBehaviour
 {
     public Material original;
     public Material night;
-    public GameObject day1;
-    public GameObject day2;
-    public bool is_off;
+    public AudioSource cancelled;
+    //Day
+    public GameObject day_obj; //christmas
+    public GameObject reindeer;
+    public GameObject drone;
+    public GameObject chandelier_main;
+    public Material chandelier_emission;
+    //Chandelier Emissions
+    //L hallway lights
+
+    //Night
+    public GameObject halloween;
+    //Irr
+    public bool is_night;
     void Start()
     {
-        is_off = false;
+        is_night = false;
         original = RenderSettings.skybox;
+        chandelier_emission = chandelier_main.GetComponent<Renderer>().sharedMaterial;
     }
 
     
-    public void change_night()
+    public void button_pressed(CollisionNotifier.EventData data)
     {
-        is_off=!is_off;
-        if(is_off)
+        Debug.Log("Code Run!");
+        is_night=!is_night;
+        if(is_night)
         {
-            day1.SetActive(false);
-            day2.SetActive(false);
+            StartCoroutine(process_item());
+            if(!cancelled.isPlaying)
+            {
+                cancelled.Play();
+            }
+            //Day Objects
+            day_obj.SetActive(false);
+            reindeer.SetActive(false);
+            drone.SetActive(false);
+            chandelier_emission.EnableKeyword("_EMISSION");
+            //Emissions
+            
+            //Night Objects
+            halloween.SetActive(true);
             RenderSettings.skybox = night;
         }
         else
         {
-            day1.SetActive(true);
-            day2.SetActive(true);
+            StartCoroutine(process_item());
+            //Day Objects
+            day_obj.SetActive(true);
+            reindeer.SetActive(true);
+            drone.SetActive(true);
+            chandelier_emission.DisableKeyword("_EMISSION");
+            
+            //Night Objects
+            halloween.SetActive(false);
             RenderSettings.skybox = original;
         }
+        
     }
+    public IEnumerator process_item()
+    {
+        yield return new WaitWhile (()=> cancelled.isPlaying);
+        yield return new WaitForSeconds(5);
+        
+     }
     
 }
