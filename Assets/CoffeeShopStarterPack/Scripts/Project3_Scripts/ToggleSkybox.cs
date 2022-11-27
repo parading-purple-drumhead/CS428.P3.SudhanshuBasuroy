240acf8471efafbb03866c539571af2c38f8ca80
Advait Pai <67473396+advaitpai@users.noreply.github.com>
2022-11-26 19:29:46 -0600

    Halloween voice changes

--- Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ToggleSkybox.cs
+++ Assets/CoffeeShopStarterPack/Scripts/Project3_Scripts/ToggleSkybox.cs
@@ -9,23 +9,27 @@ public class ToggleSkybox : MonoBehaviour
     public Material night;
     public AudioSource cancelled;
     //Day
-    public GameObject day_obj; //christmas
+    public GameObject day_obj; 
     public GameObject reindeer;
     public GameObject drone;
-    public GameObject chandelier_main;
-    public Material chandelier_emission;
-    //Chandelier Emissions
-    //L hallway lights
 
     //Night
     public GameObject halloween;
+    public GameObject chandelier_main;
+    public Material chandelier_emission;
+    public GameObject chandelier_light1;
+    public GameObject chandelier_light2;
+    public GameObject chandelier_light3;
     //Irr
     public bool is_night;
+    public bool was_pressed;
     void Start()
     {
         is_night = false;
         original = RenderSettings.skybox;
         chandelier_emission = chandelier_main.GetComponent<Renderer>().sharedMaterial;
+        chandelier_emission.DisableKeyword("_EMISSION");
+        was_pressed = false;
     }
 
     
@@ -33,43 +37,55 @@ public class ToggleSkybox : MonoBehaviour
     {
         Debug.Log("Code Run!");
         is_night=!is_night;
-        if(is_night)
+        if(!was_pressed)
         {
-            StartCoroutine(process_item());
-            if(!cancelled.isPlaying)
+            was_pressed = true;
+            if(is_night)
             {
-                cancelled.Play();
+                StartCoroutine(process_item());
+                if(!cancelled.isPlaying)
+                {
+                    cancelled.Play();
+                }
+                //Day Objects
+                day_obj.SetActive(false);
+                reindeer.SetActive(false);
+                drone.SetActive(false);
+                
+                //Emissions
+                
+                //Night Objects
+                halloween.SetActive(true);
+                RenderSettings.skybox = night;
+                chandelier_emission.EnableKeyword("_EMISSION");
+                chandelier_light1.SetActive(true);
+                chandelier_light1.SetActive(true);
+                chandelier_light1.SetActive(true);
             }
-            //Day Objects
-            day_obj.SetActive(false);
-            reindeer.SetActive(false);
-            drone.SetActive(false);
-            chandelier_emission.EnableKeyword("_EMISSION");
-            //Emissions
-            
-            //Night Objects
-            halloween.SetActive(true);
-            RenderSettings.skybox = night;
-        }
-        else
-        {
-            StartCoroutine(process_item());
-            //Day Objects
-            day_obj.SetActive(true);
-            reindeer.SetActive(true);
-            drone.SetActive(true);
-            chandelier_emission.DisableKeyword("_EMISSION");
-            
-            //Night Objects
-            halloween.SetActive(false);
-            RenderSettings.skybox = original;
+            else
+            {
+                StartCoroutine(process_item());
+                //Day Objects
+                day_obj.SetActive(true);
+                reindeer.SetActive(true);
+                drone.SetActive(true);
+                
+                //Night Objects
+                halloween.SetActive(false);
+                RenderSettings.skybox = original;
+                chandelier_emission.DisableKeyword("_EMISSION");
+                chandelier_light1.SetActive(false);
+                chandelier_light1.SetActive(false);
+                chandelier_light1.SetActive(false);
+            }
         }
         
     }
     public IEnumerator process_item()
     {
-        yield return new WaitWhile (()=> cancelled.isPlaying);
+        // yield return new WaitWhile (()=> cancelled.isPlaying);
         yield return new WaitForSeconds(5);
+        was_pressed = false;
         
      }
     
